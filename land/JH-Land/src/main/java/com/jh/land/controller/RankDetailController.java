b77364ee4a0c18e55c7494ab5fae328f72a41bee
Hilda-Wong <3167506140@qq.com>
2020-03-26 00:46:40 +0800

    查询地块信息

--- land/JH-Land/src/main/java/com/jh/land/controller/RankDetailController.java
+++ land/JH-Land/src/main/java/com/jh/land/controller/RankDetailController.java
@@ -1,5 +1,6 @@
 package com.jh.land.controller;
 
+import com.jh.land.entity.RankDetail;
 import com.jh.land.service.IRankDetailService;
 import com.jh.land.service.IRankInfoService;
 import com.jh.vo.ResultMessage;
@@ -10,6 +11,11 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+import java.time.temporal.TemporalAdjusters;
 
 @RestController
 @RequestMapping("/rankDetail")
@@ -27,9 +33,37 @@ public class RankDetailController {
     @ApiImplicitParam(name = "rankId",value = "地块ID",required = false,paramType="form",dataType = "Long")
     @RequestMapping(value="/rankDetailForBase",method= RequestMethod.GET)
     public ResultMessage rankDetailForBase(Long rankId){
-
         return  rankDetailService.rankDetailForBase(rankId);
     }
+\
+    @ApiOperation(value="",notes="根据地块id和年份查询地块的概况")
+    @ApiImplicitParam(name = "rankDetail",value = "地块详情主体",required = false,paramType="form",dataType = "RankDetail")
+    @RequestMapping(value="/rankDetailForBasicFacts",method= RequestMethod.GET)
+    public ResultMessage rankDetailForBasicFacts(@RequestBody RankDetail rankDetail){
+        return  rankDetailService.rankDetailForBasicFacts(rankDetail);
+    }
 
+    @ApiOperation(value="",notes="根据地块id和年份、月份查询长势信息")
+    @ApiImplicitParam(name = "rankDetail",value = "地块详情主体",required = false,paramType="form",dataType = "RankDetail")
+    @RequestMapping(value="/rankDetailForGrowth",method= RequestMethod.GET)
+    public ResultMessage rankDetailForGrowth(@RequestBody RankDetail rankDetail){
+        return  rankDetailService.rankDetailForGrowth(rankDetail);
+    }
+
+    @ApiOperation(value="",notes="根据地块id和年份、月份查询气象信息")
+    @ApiImplicitParam(name = "rankDetail",value = "地块详情主体",required = false,paramType="form",dataType = "RankDetail")
+    @RequestMapping(value="/rankDetailForMeteorologicalPhenomena",method= RequestMethod.GET)
+    public ResultMessage rankDetailForMeteorologicalPhenomena(@RequestBody RankDetail rankDetail){
+        return  rankDetailService.rankDetailForMeteorologicalPhenomena(rankDetail);
+    }
+    public static void main(String[] args){
+        LocalDate date = LocalDate.parse("2019-08", DateTimeFormatter.ofPattern("yyyy-MM"));
+        LocalDate first = date.with(TemporalAdjusters.firstDayOfMonth());
+        LocalDate last = date.with(TemporalAdjusters.lastDayOfMonth());
+        long days = first.until(last, ChronoUnit.DAYS);
+        System.out.println(days);
+        System.out.println(first.format(DateTimeFormatter.ofPattern("yyyy-MM-dd")));
+        System.out.println(last.format(DateTimeFormatter.ofPattern("yyyy-MM-dd")));
+    }
 
 }
